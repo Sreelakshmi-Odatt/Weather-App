18d4dcec2834399495fec817265649e7e9220478
Sreelakshmi-Odatt <sreelakshmiodatt@gmail.com>
2023-12-19 20:15:30 -0500

    Weather details  web application
    A simple weather details  web application using html , css and javascript

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Weather--Details
-A simple weather details project using html , CSS and JavaScript.
+# Weather Details Project
+A simple web application that displays weather details using HTML, CSS, and JavaScript with an API key.
+
+## Features
+
+- Fetch and display real-time weather details.
+- Uses an API key for accessing weather data.
---
+++ script.js
@@ -0,0 +1,87 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('weather');
+const timezone = document.getElementById('timezones');
+const countryEl = document.getElementById('countries');
+const weatherForecastEl = document.getElementById('forecast');
+const windSpeedEl = document.getElementById('wind_speed'); 
+const humidityEl = document.getElementById('humidity'); 
+
+
+
+const API_KEY = 'ba9554d974f79e9ca8547dd0d8926bf'; 
+
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am.pm">${ampm}</span>`
+
+    dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+
+}, 1000);
+
+getWeatherData()
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        
+        let {latitude, longitude } = success.coords;
+
+        fetch('https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&exclude={hourly, minute}&appid={API key}').then(res => res.json()).then(data => {
+
+        console.log(data)
+        showWeatherData(data);
+        })
+
+    })
+}
+
+function showWeatherData (data){
+    let {humidity, wind_speed} = data.current;
+
+    timezone.innerHTML = data.timezone;
+    countryEl.innerHTML = data.lat + 'N ' + data.lon+'E'
+
+    currentWeatherItemsEl.innerHTML = 
+    `<div class="weather-item">
+        <div>Humidity</div>
+        <div>${humidity}%</div>
+    </div>
+   
+    <div class="weather-item">
+        <div>Wind Speed</div>
+        <div>${wind_speed}</div>
+    </div>
+
+   
+    
+    `;
+
+    let otherDayForcast = ''
+    data.daily.forEach((day, idx) => {
+        if(idx == 0){
+            currentTempEl.innerHTML = `
+            <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+            <div class="other">
+                <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+                <div class="temp">Night - ${day.temp.night}&#176;C</div>
+                <div class="temp">Day - ${day.temp.day}&#176;C</div>
+            </div>
+            
+            `
+        }
+        else{
+
+        }
+    })
+
+
+    weatherForecastEl.innerHTML = otherDayForcast;
+}
